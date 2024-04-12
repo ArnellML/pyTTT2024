4c0affd71b9b938a73dde7352257b8897ee50a76
Ttwigs <166162862+Ttwigs@users.noreply.github.com>
2024-04-12 17:47:46 +0000

    Added mechanic to check if there is any connect-three and ends the game!

--- main.py
+++ main.py
@@ -19,7 +19,41 @@ def make_move(row, col, symbol):
 # Returns true when the game is over 
 # Note: Just a stub. Doesn't work yet
 def is_game_over():
-  return False
+  is_over = False
+  
+  for i in board:
+
+    is_over = all(n == "X" for n in i)
+    if is_over:
+      print_board()
+      return True
+    is_over = all(n == "O" for n in i)
+    if is_over:
+      print_board()
+      return True
+
+  for i in range(3):
+    column = [col[i] for col in board]
+    column = set(column)
+    column = list(column)
+
+    if len(column) == 1 and column[0] == "X":
+      print_board()
+      return True
+      
+    if len(column) == 1 and column[0] == "O":
+      print_board()
+      return True
+    is_over = False
+
+  if board[0][2] == board[1][1] and board[1][1] == board [2][0] and board[0][2] != "_":
+    print_board()
+    return True
+  if board[0][0] == board[1][1] and board[1][1] == board [2][2] and board[0][0] != "_":
+    print_board()
+    return True
+    
+  return is_over
 
 # Alternates the turn between 0 and 1
 def change_turn():
@@ -33,7 +67,7 @@ while not is_game_over():
   
   # Get the user input
   row_choice = int(input('Which row would you like to choose? '))
-  col_choice = int(input('Which row would you like to choose? '))
+  col_choice = int(input('Which Column would you like to choose? '))
   
   # Put their move on the board
   make_move(row_choice, col_choice, symbols[turn])
